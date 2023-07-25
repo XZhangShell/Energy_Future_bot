49a7184be7a8ae188192219af670aeda456fd2ea
XZhang <46434568+XZhangShell@users.noreply.github.com>
2023-07-25 00:05:02 -0700

    Update App_Energy_Future.py

--- App_Energy_Future.py
+++ App_Energy_Future.py
@@ -190,7 +190,7 @@ if check_password():
         template=
         """
         As an AI with extensive knowledge, I'm interested in understanding more about global energy trends, specifically regarding {topic}. There are several aspects of this that I would like you to consider:
-        1. Can you provide an analysis on the {topic}? Please use what you learn from International Energy Agency and IOC Shell, TOTAL and BP etc's published reports to answer this.
+        1. Can you provide an analysis on the {topic}? Please use what you learn from International Energy Agency and published reports from IOC Shell, TOTAL and BP etc. to answer this.
         2. Taking into account the current energy trends on the {topic}, what are the predictions for {topic} in the year 2035 and up until the end of the decade?
         3. Utilizing your predictive capabilities, can you provide an estimation of {topic} in the year 2035 and up until the end of the decade?
         4. Can you identify any significant shifts in industry trends related to {topic} for the next decade?
@@ -344,8 +344,9 @@ if check_password():
                         st.info(wiki_memory.buffer)
             # Enrich with Google Scholar research and summarization
             scholar_placeholder = st.empty()
-
+            
             scholar_research = fetch_google_scholar_research(prompt, 10)
+            scholar_placeholder.success('AI-generated scenario enriched with Google Scholar research.')
             summarized_research = summarize_text(scholar_research)
             scholar_prompt = scholar_prompt_template.format(topic=prompt)
             enriched_prompt_scholar = f"{scholar_prompt}\n{summarized_research}"
@@ -354,7 +355,7 @@ if check_password():
             if enriched_result_scholar:
                 enriched_prompt_scholar = f"{scholar_prompt}\n{summarized_research}"
                 display_answer(enriched_result_scholar, [{'title': paper['title'], 'url': paper['url'], 'abstract': paper['abstract']} for paper in scholar_research])
-                scholar_placeholder.success('AI-generated scenario enriched with Google Scholar research.')
+                
                 with st.expander('scholar History'): 
                     st.info(scholar_memory.buffer)
 
